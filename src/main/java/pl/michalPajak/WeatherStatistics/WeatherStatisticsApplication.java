19f08b58633549c3ef604f6d2d38003d23168cd3
spidomisiek88 <michal.pajak88@gmail.com>
2019-03-15 10:00:07 +0100

    first commit

---
+++ src/main/java/pl/michalPajak/WeatherStatistics/WeatherStatisticsApplication.java
@@ -0,0 +1,13 @@
+package pl.michalPajak.WeatherStatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherStatisticsApplication.class, args);
+	}
+
+}
