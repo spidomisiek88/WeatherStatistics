80e3862cc5a8d236bd8b2c1be045eb9dd5df9623
spidomisiek88 <michal.pajak88@gmail.com>
2019-03-15 15:48:25 +0100

    modiffied statiscic controller and templates

--- src/main/java/pl/michalPajak/WeatherStatistics/controllers/WeatherStatisticController.java
+++ src/main/java/pl/michalPajak/WeatherStatistics/controllers/WeatherStatisticController.java
@@ -7,8 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.michalPajak.WeatherStatistics.models.AddWeatherDataForm;
+import pl.michalPajak.WeatherStatistics.models.dto.WeatherDto;
 import pl.michalPajak.WeatherStatistics.models.services.WeatherDatabaseService;
 import pl.michalPajak.WeatherStatistics.models.services.WeatherStastisticService;
+
+import java.time.LocalDateTime;
 
 @Controller
 public class WeatherStatisticController {
@@ -43,6 +46,14 @@ public class WeatherStatisticController {
     @PostMapping("/weather/add")
     public String addWeatherDataForm(@ModelAttribute AddWeatherDataForm weatherDataForm) {
 
-        return "add_weather_data_form";
+        WeatherDto newWeather = new WeatherDto();
+        newWeather.setCityName(weatherDataForm.getCityName());
+        newWeather.setTemperature(weatherDataForm.getTemperature());
+        newWeather.setHumidity(weatherDataForm.getHumidity());
+        newWeather.setLocalDateTime(LocalDateTime.now());
+
+        weatherDatabaseService.addNewWeatherToList(newWeather);
+
+        return "redirect:/weather/add";
     }
 }
