be8b3ca15de5a0fc723228309c3d99db5ae08ade
spidomisiek88 <michal.pajak88@gmail.com>
2019-03-15 11:20:37 +0100

    added files dto, controler, templates

--- src/main/java/pl/michalPajak/WeatherStatistics/controllers/WeatherStatisticController.java
+++ src/main/java/pl/michalPajak/WeatherStatistics/controllers/WeatherStatisticController.java
@@ -1,4 +1,32 @@
 package pl.michalPajak.WeatherStatistics.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.michalPajak.WeatherStatistics.models.AddWeatherDataForm;
+
+@Controller
 public class WeatherStatisticController {
+
+    @GetMapping("/weatherStatisticView")
+    public String showWeatherStaticticView(Model model) {
+
+        return "weatherStatisticView";
+    }
+
+    @GetMapping("/addWeatherDataForm")
+    public String showAddWeatherDataForm(Model model) {
+
+        model.addAttribute("addWeatherDataForm", new AddWeatherDataForm());
+
+        return "addWeatherDataForm";
+    }
+
+    @PostMapping("/addWeatherDataForm")
+    public String addWeatherDataForm(@ModelAttribute AddWeatherDataForm weatherDataForm) {
+
+        return "addWeatherDataForm";
+    }
 }
--- src/main/java/pl/michalPajak/WeatherStatistics/models/AddWeatherDataForm.java
+++ src/main/java/pl/michalPajak/WeatherStatistics/models/AddWeatherDataForm.java
@@ -1,4 +1,63 @@
 package pl.michalPajak.WeatherStatistics.models;
 
+import java.util.Objects;
+
 public class AddWeatherDataForm {
+    private String cityName;
+    private Integer temperature;
+    private Integer humidity;
+
+    public AddWeatherDataForm() {
+        this.cityName = "";
+        this.temperature = null;
+        this.humidity = null;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public Integer getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(Integer temperature) {
+        this.temperature = temperature;
+    }
+
+    public Integer getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(Integer humidity) {
+        this.humidity = humidity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AddWeatherDataForm)) return false;
+        AddWeatherDataForm that = (AddWeatherDataForm) o;
+        return getCityName().equals(that.getCityName()) &&
+                getTemperature().equals(that.getTemperature()) &&
+                getHumidity().equals(that.getHumidity());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCityName(), getTemperature(), getHumidity());
+    }
+
+    @Override
+    public String toString() {
+        return "AddWeatherDataForm{" +
+                "cityName='" + cityName + '\'' +
+                ", temperature=" + temperature +
+                ", humidity=" + humidity +
+                '}';
+    }
 }
